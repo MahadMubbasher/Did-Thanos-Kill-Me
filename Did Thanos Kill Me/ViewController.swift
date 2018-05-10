1e649d8c4668593dcd775108c07d81d325397c46
Mahad Mubbasher <mahad@welltimes-macbook-pro.local>
2018-05-10 19:46:56 +0100

    Initial Commit

---
+++ Did Thanos Kill Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Did Thanos Kill Me
+//
+//  Created by Mahad Mubbasher on 10/05/2018.
+//  Copyright © 2018 Mahad Mubbasher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
