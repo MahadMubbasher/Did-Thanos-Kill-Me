3187ba4357996e9afca6384c397b10eae3b3864a
Mahad Mubbasher <mahad@welltimes-macbook-pro.local>
2018-06-10 00:02:35 +0100

    Develop the animation in a separate project, then copy and paste it over here.

--- Did Thanos Kill Me/ViewController.swift
+++ Did Thanos Kill Me/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var cameraView: AACameraView!
     @IBOutlet weak var killButton: UIButton!
-        
+    //@IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -76,66 +77,123 @@ class ViewController: UIViewController {
     
     //MARK : Splitting the image.
     
-//    func splitImage(row : Int , column : Int) {
-//
-//        let oImg = imageView.image
-//
-//        let height =  (imageView.image?.size.height)! /  CGFloat (row) //height of each image tile
-//        let width =  (imageView.image?.size.width)!  / CGFloat (column)  //width of each image tile
-//
-//        let scale = (imageView.image?.scale)! //scale conversion factor is needed as UIImage make use of "points" whereas CGImage use pixels.
-//
-//        var imageArr = [[UIImage]]() // will contain small pieces of image
-//
-//        for y in 0..<row {
-//
-//            var yArr = [UIImage]()
-//
-//            for x in 0..<column{
-//
-//                UIGraphicsBeginImageContextWithOptions(
-//                    CGSize(width:width, height:height),
-//                    false, 0)
-//                let i =  oImg?.cgImage?.cropping(to:  CGRect.init(x: CGFloat(x) * width * scale, y:  CGFloat(y) * height * scale  , width: width * scale  , height: height * scale) )
-//
-//                let newImg = UIImage.init(cgImage: i!)
-//
-//                yArr.append(newImg)
-//
-//                UIGraphicsEndImageContext()
-//
-//            }
-//
-//            imageArr.append(yArr)
-//        }
-//
-//    }
-//
-//    func createNewImage(imageArr : [[UIImage]]) {
-//
-//        let row = imageArr.count
-//        let column = imageArr[0].count
-//        let height =  (imageView.frame.size.height) /  CGFloat (row )
-//        let width =  (imageView.frame.size.width) / CGFloat (column )
-//
-//
-//        UIGraphicsBeginImageContext(CGSize.init(width: imageView.frame.size.width , height: imageView.frame.size.height))
-//
-//        for y in 0..<row{
-//
-//            for x in 0..<column{
-//
-//                let newImage = imageArr[y][x]
-//
-//                newImage.draw(in: CGRect.init(x: CGFloat(x) * width, y:  CGFloat(y) * height  , width: width - 1  , height: height - 1 ))
-//
-//            }
-//        }
-//
-//        let originalImg = UIGraphicsGetImageFromCurrentImageContext()
-//        UIGraphicsEndImageContext();
-//        imageView.image = originalImg
-//    }
+    var imageArr = [[UIImage]]() //will contain small pieces of image //Note: this is an array of arrays
+    
+    
+    //    func moveRight(view: UIImage) {
+    //
+    //        imgImage.frame.origin.x += 300
+    //    }
+    
+    func splitImage(row : Int , column : Int) {
+        
+        let oImg = imageView.image
+        
+        var height =  (imageView.image?.size.height)! /  CGFloat (row) //height of each image tile
+        var width =  (imageView.image?.size.width)!  / CGFloat (column)  //width of each image tile
+        
+        let scale = (imageView.image?.scale)! //scale conversion factor is needed as UIImage make use of "points" whereas CGImage use pixels.
+        
+        //            var imageArr = [[UIImage]]() //will contain small pieces of image //Note: this is an array of arrays
+        
+        for y in 0..<row {
+            
+            var yArr = [UIImage]()
+            
+            for x in 0..<column{
+                
+                UIGraphicsBeginImageContextWithOptions(
+                    CGSize(width:width, height:height),
+                    false, 0)
+                let i =  oImg?.cgImage?.cropping(to:  CGRect.init(x: CGFloat(x) * width * scale, y:  CGFloat(y) * height * scale  , width: width * scale  , height: height * scale) )
+                
+                let newImg = UIImage.init(cgImage: i!)
+                
+                yArr.append(newImg)
+                
+                UIGraphicsEndImageContext()
+                
+            }
+            
+            imageArr.append(yArr)
+            
+        }
+        
+        print("rows: \(row), cols: \(column), imageArr.count: \(imageArr.count), imageArr[0].count: \(imageArr[0].count)")
+        
+        
+        // Create New Image from split tiles in Array
+        let row = imageArr.count
+        let column = imageArr[0].count
+        
+        height =  (imageView.frame.size.height) /  CGFloat (row )
+        width =  (imageView.frame.size.width) / CGFloat (column )
+        
+        print("For forward block creation from row: \(row), col: \(column), height: \(height), width: \(width)")
+        
+        UIGraphicsBeginImageContext(CGSize.init(width: imageView.frame.size.width , height: imageView.frame.size.height))
+        
+        for y in 0..<row{
+            
+            for x in 0..<column{
+                
+                let newImage = imageArr[y][x]
+                
+                newImage.draw(in: CGRect.init(x: CGFloat(x) * width, y:  CGFloat(y) * height  , width: width - 1  , height: height - 1 ))
+                
+                print("Values for imageArr[\(y)][\(x)]): ", imageArr[y][x])
+            }
+        }
+        
+        let originalImg = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        imageView.image = originalImg
+        
+        
+        // MARK: Animate Disintegrating
+        
+        print("For reverse block creation from row: \(row), col: \(column)")
+        // reverse count for last parts of the image to disintegrate first- goes to index 0 (using to: -1 in stride)
+        
+        for r in stride(from: row - 1, to: -1, by: -1) {
+            print("r: ",r)
+            
+            for c in stride(from: column - 1, to: -1, by: -1) {
+                print("c: ",c)
+                
+                print("index in imageArr[\(r)][\(c)] is:", imageArr[r][c])
+                
+                //The disintegration block and Adding the image to UIimageview
+                
+                let imgBlock = UIImageView(frame: CGRect(x: CGFloat(r)+width , y: CGFloat(c), width: width, height: height))
+                
+                imgBlock.contentMode = .scaleAspectFit
+                imgBlock.image = imageArr[r][c]
+                //                    imgBlock.image = imageArr[r].last
+                view.addSubview(imgBlock)
+                
+                let delay : Double = 0.25
+                let duration : Double = 2.0
+                
+                UIView.animate(withDuration: duration, delay: delay, options: .curveLinear, animations: {
+                    imgBlock.center.x += 350
+                    
+                }) { (_) in
+                    
+                }
+                
+                
+            }
+        }
+        UIGraphicsEndImageContext()
+        
+    }
+    
+    @IBAction func splitImagePressed(_ sender: Any) {
+        
+        splitImage(row: 3, column: 5)
+        //createNewImage(imageArr: [[UIImage]]())
+    }
     
     //MARK : Moving the image.
     
